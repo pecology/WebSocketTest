ae8260785bee40f7671ceb1e867ad46ca2b3390c
pecology <dummy>
2021-04-25 13:01:56 +0900

    コンソールに入力した文字を送信し、それをそのまま受信するプログラム

---
+++ WebSocketClient/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebSocketClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync().Wait();
+        }
+
+        private async static Task MainAsync()
+        {
+            var socket = new ClientWebSocket();
+            var uri = new Uri("ws://localhost:60304/webSocketTest");
+            await socket.ConnectAsync(uri, CancellationToken.None);
+
+            Console.Write("Type send text: ");
+            var sendText = Console.ReadLine();
+
+            var sendBuffer = Encoding.UTF8.GetBytes(sendText);
+            await socket.SendAsync(sendBuffer.AsMemory(), WebSocketMessageType.Text, true, CancellationToken.None);
+
+            var recvBuffer = new byte[8];
+            await socket.ReceiveAsync(recvBuffer.AsMemory(), CancellationToken.None);
+            var recvText = Encoding.UTF8.GetString(recvBuffer);
+            Console.WriteLine("recv > " + recvText); 
+
+            //while(true)
+            //{
+            //    var segment = new ArraySegment<byte>(buffer);
+            //    var result = await socket.ReceiveAsync(segment, CancellationToken.None);
+
+            //    if (result.MessageType == WebSocketMessageType.Close)
+            //    {
+            //        await socket.CloseAsync(WebSocketCloseStatus.NormalClosure, "OK", CancellationToken.None);
+            //        return;
+            //    }
+
+            //    if (result.MessageType == WebSocketMessageType.Binary)
+            //    {
+            //        await socket.CloseAsync(WebSocketCloseStatus.InvalidMessageType, "binary not suport", CancellationToken.None);
+            //        return;
+            //    }
+
+            //    var count = result.Count;
+            //    while (!result.EndOfMessage)
+            //    {
+            //        if (count >= buffer.Length)
+            //        {
+            //            await socket.CloseAsync(WebSocketCloseStatus.InvalidPayloadData, "too big", CancellationToken.None);
+
+            //            return;
+            //        }
+
+            //        segment = new ArraySegment<byte>(buffer, count, buffer.Length - count);
+            //        result = await socket.ReceiveAsync(segment, CancellationToken.None);
+            //        count += result.Count;
+            //    }
+
+            //    var message = Encoding.UTF8.GetString(buffer, 0, count);
+            //    Console.WriteLine("> " + message);
+            //}
+        }
+    }
+}
--- WebSocketTest/Startup.cs
+++ WebSocketTest/Startup.cs
@@ -1,9 +1,12 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.WebSockets;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -52,6 +55,47 @@ namespace WebSocketTest
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            app.UseWebSockets();
+
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path == "/webSocketTest")
+                {
+                    if (context.WebSockets.IsWebSocketRequest)
+                    {
+                        using (var webSocket = await context.WebSockets.AcceptWebSocketAsync())
+                        {
+                            await Echo(context, webSocket);
+                        }
+
+                    }
+                    else
+                    {
+                        context.Response.StatusCode = 400;
+                    }
+                }
+                else
+                {
+                    await next();
+                }
+
+            });
+        }
+
+        private async Task Echo(HttpContext context, WebSocket webSocket)
+        {
+            var buffer = new byte[1024 * 4];
+            var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+            while (!result.CloseStatus.HasValue)
+            {
+                await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
+
+                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            }
+
+            await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
         }
     }
 }
