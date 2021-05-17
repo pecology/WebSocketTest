6d22d87af38e15b46e4adfcab08413d34bd84bd8
pecology <dummy>
2021-05-17 23:05:34 +0900

    コンソールでWebSocketリクエストを受け入れるサーバーを作成(管理者権限で実行する必要あり)

--- WebSocketClient/Program.cs
+++ WebSocketClient/Program.cs
@@ -20,45 +20,14 @@ namespace WebSocketClient
         private async static Task MainAsync()
         {
             var socket = new ClientWebSocket();
-            var uri = new Uri("ws://localhost:60304/webSocketTest");
-            await socket.ConnectAsync(uri, CancellationToken.None);
+            var uri = new Uri("ws://localhost:80/");
+                await socket.ConnectAsync(uri, CancellationToken.None);
 
-            var messageQueue = new ConcurrentQueue<byte[]>();
-
-            // キューにあるデータを送信し続けるタスク
-            var sendTask = Task.Run(async () =>
+            for (int i = 0; i < 100; i++)
             {
-                while (true)
-                {
-                    if (messageQueue.TryDequeue(out var message))
-                    {
-                        // キューにデータがあれば送信
-                        await socket.SendAsync(message, WebSocketMessageType.Binary, true, CancellationToken.None);
-                    }
-                    else
-                    {
-                        // キューにデータがなければ0.1秒待つ
-                        await Task.Delay(100);
-                    }
-                }
-            });
-
-            // 一秒毎にキューにあるデータの数をログ出力
-            var logTask = Task.Run(async () =>
-            {
-                while (true)
-                {
-                    await Task.Delay(1000);
-                    await Console.Out.WriteLineAsync("queue data count: " + messageQueue.Count);
-                }
-            });
-
-            // 0.1秒に一回1MBのデータをキューに入れる。
-            var sendData = new byte[1024 * 1024];
-            while(true)
-            {
-                await Task.Delay(100);
-                messageQueue.Enqueue(sendData);
+                var data = new byte[1];
+                var res = await socket.ReceiveAsync(data, CancellationToken.None);
+                Console.WriteLine(data[0]);
             }
         }
     }
---
+++ WebSocketConsoleServer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebSocketConsoleServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync().Wait();
+        }
+
+        static async Task MainAsync()
+        {
+
+            Console.WriteLine("Hello World!");
+            HttpListener listener = new HttpListener();
+            listener.Prefixes.Add("http://localhost:80/");
+            listener.Start();
+
+            var context = await listener.GetContextAsync();
+            var webSocketContext = await context.AcceptWebSocketAsync(null);
+
+            var webSocket = webSocketContext.WebSocket;
+
+            for (int i = 0; i < 100; i++)
+            {
+                var sendData = new byte[1];
+                sendData[0] = (byte)i;
+                await webSocket.SendAsync(sendData, System.Net.WebSockets.WebSocketMessageType.Binary, true, CancellationToken.None);
+            }
+
+        }
+    }
+}
